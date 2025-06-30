734b5a0128eb4453b5f874a3959404b590086296
Prashanth Puranik <puranik@digdeeper.in>
2025-06-30 16:47:43 +0530

    STEP 3

--- demos/01-angular/workshops-app/src/app/app.ts
+++ demos/01-angular/workshops-app/src/app/app.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgbAlert, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'Hello Angular v20!';
-  count = 0;
+  protected title : string = 'Hello Angular v20!';
+  count : number = 0;
+  isOpen = true;
 
   increment() {
     this.count = this.count + 1;
+  }
+
+  toggle() {
+    // alert( 'closed' );
+    this.isOpen = false;
   }
 }
