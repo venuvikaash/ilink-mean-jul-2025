6d46161ee5066528a8a4fa364cd6d0b793e096aa
Prashanth Puranik <puranik@digdeeper.in>
2025-07-01 17:07:33 +0530

    STEP 11

--- demos/01-angular/workshops-app/src/app/workshops/workshops-list/workshops-list.ts
+++ demos/01-angular/workshops-app/src/app/workshops/workshops-list/workshops-list.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 import { WorkshopsService } from '../workshops';
 import IWorkshop from '../models/IWorkshop';
 
 @Component({
   selector: 'app-workshops-list',
   imports: [
-    CommonModule
+    CommonModule,
+    NgbAlert
   ],
   templateUrl: './workshops-list.html',
   styleUrl: './workshops-list.scss'
 })
 export class WorkshopsList implements OnInit {
   workshops!: IWorkshop[];
+  error: Error | null = null;
+  loading: boolean = true;
 
   // short syntax for data member creation and initialization
   constructor(private workshopsService : WorkshopsService) {
@@ -21,14 +25,22 @@ export class WorkshopsList implements OnInit {
 
   // Lifecycle method: executed when the component shows up on the screen
   ngOnInit() {
+    this.loading = true;
+
     this.workshopsService.getWorkshops().subscribe(
       {
+        // called when operation (backend call) is successful - it is passed the data
         next: ( w ) => {
           console.log( w );
           this.workshops = w;
+          this.loading = false;
         },
+
+        // called when operation is unsuccessful - it is passed the error
         error: ( err ) => {
           console.log( err );
+          this.loading = false;
+          this.error = err;
         }
       }
     );
