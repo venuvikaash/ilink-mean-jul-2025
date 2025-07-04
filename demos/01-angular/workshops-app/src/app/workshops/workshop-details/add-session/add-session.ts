5b8e27be06281773c495734bccf3e08217b3030c
Prashanth Puranik <puranik@digdeeper.in>
2025-07-04 17:50:17 +0530

    STEP 35 - submitting new session t the backend

--- demos/01-angular/workshops-app/src/app/workshops/workshop-details/add-session/add-session.ts
+++ demos/01-angular/workshops-app/src/app/workshops/workshop-details/add-session/add-session.ts
@@ -103,34 +103,34 @@ export class AddSession {
       'id'
     ) as string);
 
-    // const newSession = {
-    //   ...addSessionForm.value,
-    //   workshopId: id,
-    //   upvoteCount: 0,
-    //   sequenceId: +addSessionForm.value.sequenceId,
-    //   duration: +addSessionForm.value.duration,
-    // } as Omit<ISession, 'id'>;
+    const newSession = {
+      ...addSessionForm.value,
+      workshopId: id,
+      upvoteCount: 0,
+      sequenceId: +addSessionForm.value.sequenceId,
+      duration: +addSessionForm.value.duration,
+    } as Omit<ISession, 'id'>;
 
-    // console.log(newSession);
+    console.log(newSession);
 
-    // this.sessionsService.addSession(newSession).subscribe({
-    //   next: (addedSession) => {
-    //     this.toastService.add({
-    //       message: `Added session with id = ${addedSession.id}`,
-    //       className: 'bg-success text-light',
-    //       duration: 5000,
-    //     });
+    this.sessionsService.addSession(newSession).subscribe({
+      next: (addedSession) => {
+        this.toastService.add({
+          message: `Added session with id = ${addedSession.id}`,
+          className: 'bg-success text-light',
+          duration: 5000,
+        });
 
-    //     // You can also use navigateByUrl()
-    //     this.router.navigate(['/workshops', id]);
-    //   },
-    //   error: (error) => {
-    //     this.toastService.add({
-    //       message: `Unable to add the session - ${error.message}`,
-    //       className: 'bg-danger text-light',
-    //       duration: 5000,
-    //     });
-    //   },
-    // });
+        // You can also use navigateByUrl()
+        this.router.navigate(['/workshops', id]);
+      },
+      error: (error) => {
+        this.toastService.add({
+          message: `Unable to add the session - ${error.message}`,
+          className: 'bg-danger text-light',
+          duration: 5000,
+        });
+      },
+    });
   }
 }
