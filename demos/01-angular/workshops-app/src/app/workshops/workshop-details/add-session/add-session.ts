b8fc98a17d288cdc8c963f2cde50d8da4fc11eb8
Prashanth Puranik <puranik@digdeeper.in>
2025-07-04 14:55:53 +0530

    Step 31: Create the form to add a session

--- demos/01-angular/workshops-app/src/app/workshops/workshop-details/add-session/add-session.ts
+++ demos/01-angular/workshops-app/src/app/workshops/workshop-details/add-session/add-session.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-session',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './add-session.html',
   styleUrl: './add-session.scss'
 })
