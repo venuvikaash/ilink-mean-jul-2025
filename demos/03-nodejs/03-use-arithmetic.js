ef677a6d11cf4562b651be3dac646a4a211cbfa9
Prashanth Puranik <puranik@digdeeper.in>
2025-07-10 16:04:06 +0530

    modules

--- demos/03-nodejs/03-use-arithmetic.js
+++ demos/03-nodejs/03-use-arithmetic.js
@@ -3,6 +3,7 @@ console.log( '[3] use-arithmetic.js started' );
 // we get the module.exports fro arithmetic.js
 const arithmetic = require("./03-arithmetic");
 
+// Note that multiply is not available
 console.log( arithmetic );
 
 console.log( arithmetic.addArray( [ 1, 2, 3, 4 ] ) );
