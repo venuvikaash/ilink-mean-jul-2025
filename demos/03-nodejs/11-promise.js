941910d869007727a85baab9a9def3e1f239eaa2
Prashanth Puranik <puranik@digdeeper.in>
2025-07-10 18:20:35 +0530

    promise chaining internals

--- demos/03-nodejs/11-promise.js
+++ demos/03-nodejs/11-promise.js
@@ -21,25 +21,25 @@ function sum( x, y ) {
 // using sum()
 // then(), catch() -> 2 methods of Promise class
 // the moment Promise object gets the resolved value (answer), the function passed to then (f) is called
-sum( 1, 2 )
+sum( 1, 2 ) // p1
     .then(
         function( result1 ) { // f
             console.log( result1 );
 
-            return sum( result1, 3 ); // NOTE: Not to forget to return the promise
+            return sum( result1, 3 ); // NOTE: Not to forget to return the promise (p5) -> after some time when we get data 6
         }
-    )
+    ) // p2 (p2 is a copycat of p5) -> p2 will also resolve with value 6
     .then(
         function( result2 ) {
             console.log( result2 );
 
             return sum( result2, 4 );
         }
-    )
+    ) // p3
     .then(
         function( result3 ) {
             console.log( result3 );
         }
-    );
+    ); // p4
 
 console.log( 'script ends' );
