3fcf2eea4e857efff12b1a56f3e35784b285110e
Prashanth Puranik <puranik@digdeeper.in>
2025-07-14 15:33:16 +0530

    STEP 6 (Partial)

--- demos/04-express/workshops-app/src/routes/workshops.route.js
+++ demos/04-express/workshops-app/src/routes/workshops.route.js
@@ -7,4 +7,9 @@ router.get( '/workshops', ( req, res ) => {
     res.json( workshops ); // res.json( js_array / js_object )
 });
 
+router.post( '/workshops', ( req, res ) => {
+    // 'Content-Type': 'text/html' (send() sets this HTTP header, end does not)
+    res.send( 'Hello Postman' );
+});
+
 module.exports = router;
